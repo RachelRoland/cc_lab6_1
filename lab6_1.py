666218316b4cd124299c19fe3bba7d4471bcf4ec
Rachel Roland <rachelroland@Shawns-Air.hsd1.wa.comcast.net>
2020-10-18 14:36:40 -0700

    homework

--- lab6_1.py
+++ lab6_1.py
@@ -1,8 +1,16 @@
-# TODO: Write a function called divisible_by_7. 
+# Write a function called divisible_by_7. 
 # This function must take an integer as a parameter.
 # This function must return True if the passed integer
 # is divisible by 7 or false if not.
 # Note: you should not prompt the user for input. 
+def divisible_by_7(x):
+   #divisible by 7 
+   if x % 7 == 0:
+      return True 
+   # not divisible by 7 
+   return false 
+
+
 
 
 # TODO: Write a function called compare_it that takes two parameters. 
@@ -13,18 +21,43 @@
 # Finally, test if the parameters are greater than zero.
 # If they are not, return False.
 # If all of these tests pass, return True.
+def compare_it(x, y):
+   #test they're integers (negative test) 
+   if not isinstance(x, int) and not isinstance(y, int): 
+      return False 
+   
+   #test if equal (return false) 
+   if x != y:
+      return False 
+   #test if greater than zero (return false) 
+   if x <= 0 and y <= 0: 
+      return False 
+   #all tests pass return true 
+   return True 
 
 
 # TODO: Write a function called keyword_counter that takes three parameters
+def keyword_counter(words, string, filename):
 # Parameter one should be a list of words.
+   words = ('')
+   
 # The second should be set to None by default, and could hold a string to search.
+   string = 
+
 # The third should be set to None by default, and could hold a filename to search.
+   filename = 
+
 # If a string and a filename are both passed to the program, the program should 
 # return None.
+
+return None 
+
 # If the function is passed a string in the second parameter, then it should
 # search the string.
+
 # If the function is passed a filename in the third parameter, then it should
 # read the file and search the contents of the file.
+
 # Unless the function is passed both a string and a filename, your function 
 # must return a dictionary with the keywords as the key and the count of 
 # occurrences of that keyword as the value as follows:
